907d56985392cfd7e3b21354bcc9c571eec6a06b
neolao <contact@neolao.com>
2022-01-06 13:18:51 +0100

    up

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # portable-ide
+
+sudo cp /lib/terminfo/x/xterm-256color /usr/share/terminfo/x/xterm-256color
--- ide.sh
+++ ide.sh
@@ -5,5 +5,38 @@ currentDirectory=$(dirname $scriptPath)
 
 export VIMRUNTIME=$currentDirectory/runtime
 
-./nvim.appimage -u $currentDirectory/config/vimrc.vim $@
-#./squashfs-root/usr/bin/nvim -u $currentDirectory/config/vimrc.vim $@
+APPIMAGE_OPTIONS=''
+TMUX_ENABLED=0
+TARGET='.'
+while test $# -gt 0; do
+  case "$1" in
+    --extract-and-run)
+      APPIMAGE_OPTIONS='--appimage-extract-and-run'
+      shift
+      ;;
+
+    --tmux)
+      TMUX_ENABLED=1
+      shift
+      ;;
+
+    *)
+      TARGET="$1"
+      shift
+      ;;
+  esac
+done
+
+if [ "$TMUX_ENABLED" -eq 0 ]
+then
+  ./nvim-*.appimage $APPIMAGE_OPTIONS -u $currentDirectory/config/vimrc.vim $TARGET
+else
+  mkdir -p ./tmp
+  TMUX_OPTIONS="-2 -S ./tmp/tmux.pid"
+  TMUX_SESSION_NAME="ide"
+  ./tmux-*.appimage $TMUX_OPTIONS new-session -d -s $TMUX_SESSION_NAME
+  ./tmux-*.appimage $TMUX_OPTIONS new-window -t $TMUX_SESSION_NAME:1 -k -n "EDITOR" ./nvim-*.appimage $APPIMAGE_OPTIONS -u $currentDirectory/config/vimrc.vim $TARGET
+  ./tmux-*.appimage $TMUX_OPTIONS select-window -t $TMUX_SESSION_NAME:1.0
+  ./tmux-*.appimage $TMUX_OPTIONS attach -t $TMUX_SESSION_NAME
+fi
+
